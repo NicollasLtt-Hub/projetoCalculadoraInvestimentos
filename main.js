38956081faa2a49327da59db72a797112d1bd169
Nicollas Lott <nicollaslottcurvellano@outlook.com>
2025-11-14 21:54:52 -0300

    Formulario completo.

---
+++ main.js
@@ -0,0 +1,73 @@
+import { generateReturnsArray } from './src/investmentGoals.js';
+
+const investmentForm = document.getElementById('investment-form');
+const clearFormBtn = document.getElementById('clear-form');
+
+function renderProgression(e) {
+  e.preventDefault();
+  if (document.querySelector('.error')) {
+    return;
+  }
+
+  const startingAmount = Number(document.getElementById('starting-amount').value.replace(',', '.'));
+  const additionalContribution = Number(document.getElementById('additional-contribution').value.replace(',', '.'));
+  const timeAmount = Number(document.getElementById('time-amount').value);
+  const timeAmountPeriod = document.getElementById('time-amount-period').value;
+  const returnRate = Number(document.getElementById('return-rate').value.replace(',', '.'));
+  const returnRatePeriod = document.getElementById('return-rate-period').value;
+  const taxRate = Number(document.getElementById('tax-rate').value.replace(',', '.'));
+
+  const returnsArray = generateReturnsArray(
+    startingAmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribution,
+    returnRate,
+    returnRatePeriod
+  );
+
+  console.log(returnsArray);
+}
+
+function clearForm() {
+  const inputsWithName = investmentForm.querySelectorAll('input[name]');
+  inputsWithName.forEach((input) => {
+    input.value = '';
+  });
+
+  const errorInputs = document.querySelectorAll('.error');
+  errorInputs.forEach((errorContainer) => {
+    errorContainer.classList.remove('error');
+    errorContainer.parentElement.querySelector('p').remove();
+  });
+}
+
+function validateInput(e) {
+  if (e.target.value === '') {
+    return;
+  }
+
+  const parentElementHTML = e.target.parentElement;
+  const grandParentElementHTML = parentElementHTML.parentElement;
+
+  const inputValue = e.target.value.replace(',', '.');
+  if ((isNaN(inputValue) || Number(inputValue) <= 0) && !parentElementHTML.classList.contains('error')) {
+    const errorTextElement = document.createElement('p');
+    errorTextElement.classList.add('text-red-600');
+    errorTextElement.innerText = '*Insira um valor numérico e maior que zero.';
+
+    parentElementHTML.classList.add('error');
+    grandParentElementHTML.appendChild(errorTextElement);
+  } else if ((!isNaN(inputValue) || Number(inputValue) > 0) && parentElementHTML.classList.contains('error')) {
+    parentElementHTML.classList.remove('error');
+    grandParentElementHTML.querySelector('p').remove();
+  }
+}
+
+for (const formElement of investmentForm) {
+  if (formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+    formElement.addEventListener('blur', validateInput);
+  }
+}
+investmentForm.addEventListener('submit', renderProgression);
+clearFormBtn.addEventListener('click', clearForm);
